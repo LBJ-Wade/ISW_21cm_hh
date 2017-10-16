a66ca9ae81bdda92be5269a1d5337fd0bcaeee8f
NanoomLee <NanoomLee@yinanoomui-MacBook-Pro.local>
2017-10-16 16:04:27 -0400

    Changes path for data

--- path.py
+++ path.py
@@ -3,6 +3,6 @@ path_CLASS_syn = default + "/ISW_21cm/class_syn"
 path_CLASS_new = default + "/ISW_21cm/class_new"
 path_HYREC = default + "/ISW_21cm/Hyrec"
 path_result = default + "/ISW_21cm/result"
-path_data = default + "/data"
+path_data = default + "/ISW_21cm/data"
 params_input = "params_nonu.dat"
 
