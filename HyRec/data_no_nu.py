da738d70c69acb4d3c340d571a46b7e547372c56
NanoomLee <NanoomLee@yinanoomui-MacBook-Pro.local>
2017-10-18 13:31:50 -0400

    Deleted

--- HyRec/data_no_nu.py
+++
@@ -1,8 +0,0 @@
-params = 'nanoom'
-
-transfer_new = "params/delta_no_nu_nanoom_new.dat"
-transfer_syn = "params/delta_no_nu_nanoom.dat"
-transfer_21 = "transfer_21_nonu.txt"
-power_spectrum = "params/params_no_nu_nanoom_00_pk.dat"
-cl = "params/params_no_nu_nanoom_00_cl.dat"
-
