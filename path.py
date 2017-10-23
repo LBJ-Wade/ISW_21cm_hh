8539f3056616ff146b3619e453f35326effc6f6e
NanoomLee <NanoomLee@yinanoomui-MBP.home>
2017-10-23 19:42:02 -0400

    fixed path for HyRec

--- path.py
+++ path.py
@@ -1,7 +1,7 @@
 default = "/Users/NanoomLee/Documents/Master@StonyBrook/ISW_21cm_Code/ISW_21cm"
 path_CLASS_syn = default + "/class_syn"
 path_CLASS_new = default + "/class_new"
-path_HYREC = default + "/Hyrec"
+path_HYREC = default + "/HyRec"
 path_result = default + "/result"
 path_data = default + "/data"
 params_input = default + "/params_nonu.dat"
