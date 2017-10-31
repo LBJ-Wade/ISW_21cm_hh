a5503054cdd5eb09cd9e3deaacb3836217d75869
NanoomLee <nanoom.lee@stonybrook.edu>
2017-10-31 19:40:14 -0400

    .

--- fisher_cmb.py
+++ fisher_cmb.py
@@ -5,7 +5,7 @@ F.cmb_deriv_vec ()
 F.cov_matrix ()
 fisher_matrix = F.cmb_fisher_analysis ()
 
-data = np.column_stack((fisher_matrix[0],fisher_matrix[1],fisher_matrix[2],fisher_matrix[3],fisher_matrix[4],fisher_matrix[5],fisher_matrix[6],fisher_matrix[7]))
+data = np.column_stack((fisher_matrix[0],fisher_matrix[1],fisher_matrix[2],fisher_matrix[3],fisher_matrix[4],fisher_matrix[5],fisher_matrix[6]))
 np.savetxt('fisher_matrix.txt', data, fmt = '%1.6e')
 
 inv_fisher = inv(fisher_matrix)
