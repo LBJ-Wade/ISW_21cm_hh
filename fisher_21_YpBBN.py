45df02c526b74311d7d4b15d9424db4c58bb7852
NanoomLee <nanoom.lee@stonybrook.edu>
2017-11-16 22:53:32 -0500

    new running files

---
+++ fisher_21_YpBBN.py
@@ -0,0 +1,20 @@
+from fisher import *
+# Fisher 21T
+F = fisher (Yp_BBN = True)
+F.cl21T_deriv_vec ()
+F.cov_matrix ()
+fisher_matrix = F.fisher_analysis ()
+
+data = np.column_stack((fisher_matrix[0],fisher_matrix[1],fisher_matrix[2],fisher_matrix[3],fisher_matrix[4],fisher_matrix[5],fisher_matrix[6],fisher_matrix[7]))
+#data = np.column_stack((fisher_matrix[0]))
+np.savetxt('fisher_matrix_cl21T.txt', data, fmt = '%1.6e')
+
+inv_fisher = inv(fisher_matrix)
+
+sigma = []
+for i in range(len(fisher_matrix)):
+	sigma.append (np.sqrt(inv_fisher[i,i]))
+sigma = np.array(sigma)
+data = np.column_stack((sigma))
+np.savetxt('sigma_cl21T.txt', data, fmt = '%1.6e')
+plt.show()
---
+++ fisher_cmb_YpBBN.py
@@ -0,0 +1,19 @@
+from fisher import *
+# Fisher CMB
+F = prior_cmb (Yp_BBN = True)
+F.cmb_deriv_vec ()
+F.cov_matrix ()
+fisher_matrix = F.cmb_fisher_analysis ()
+
+data = np.column_stack((fisher_matrix[0],fisher_matrix[1],fisher_matrix[2],fisher_matrix[3],fisher_matrix[4],fisher_matrix[5],fisher_matrix[6],fisher_matrix[7]))
+#data = np.column_stack((fisher_matrix[0],fisher_matrix[1],fisher_matrix[2],fisher_matrix[3],fisher_matrix[4],fisher_matrix[5],fisher_matrix[6]))
+np.savetxt('fisher_matrix.txt', data, fmt = '%f')
+
+inv_fisher = inv(fisher_matrix)
+
+sigma = []
+for i in range(len(fisher_matrix)):
+	sigma.append (np.sqrt(inv_fisher[i,i]))
+sigma = np.array(sigma)
+data = np.column_stack((sigma))
+np.savetxt('sigma.txt', data, fmt = '%1.6e')
